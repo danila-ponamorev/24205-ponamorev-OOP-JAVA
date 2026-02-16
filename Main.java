7c0488c53b58cd9d288bfea0f3a9e4ac2ade5184
danila-ponamorev <d.ponamorev@g.nsu.ru>
2026-02-16 14:11:08 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		// Печать "Hello wordl"
+		System.out.println("Hello world!");
+		// Печать переданных аргументов (дополнение лектора)
+		for (String s: args) {
+		// Удобная конкатенация строк
+		 System.out.println("argument: "+s);
+		}
+	} 
+}
